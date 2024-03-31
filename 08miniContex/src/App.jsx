98e722371ed0ec32cd01e7153e5df92259493f1e
RajivMahato68 <136238174+RajivMahato68@users.noreply.github.com>
2024-03-31 12:25:15 +0545

    Context Api discussion

---
+++ 08miniContex/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import UserContextProvider from './Context/UserContextProvider'
+import Profile from './Components/Profile'
+import Login from './Components/Login'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+    <h1>React with chai</h1>
+    <Login />
+    <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
